d3eb07bd1e4d5f861a438807d8d5211b68787746
mohamed <mohamed_ben_salem@outlook.com>
2021-12-31 09:11:16 -0500

    Send result

--- src/server.ts
+++ src/server.ts
@@ -14,23 +14,6 @@ const isImageURL = require('image-url-validator').default;
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
-  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
-  // GET /filteredimage?image_url={{URL}}
-  // endpoint to filter an image from a public url.
-  // IT SHOULD
-  //    1
-  //    1. validate the image_url query
-  //    2. call filterImageFromURL(image_url) to filter the image
-  //    3. send the resulting file in the response
-  //    4. deletes any files on the server on finish of the response
-  // QUERY PARAMATERS
-  //    image_url: URL of a publicly accessible image
-  // RETURNS
-  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
-
-  /**************************************************************************** */
-
-  //! END @TODO1
 
   app.get('/filteredimage', async (req, res) => {
     const  url =req.query.image_url;
@@ -38,8 +21,8 @@ const isImageURL = require('image-url-validator').default;
     if(!isValidURL) {
       return res.status(400).send("the URL of the image is not valid.")
     }
-    const  filteredpath  = filterImageFromURL(url);
-    res.send("finished")
+    const  filteredpath  = await filterImageFromURL(url);
+    res.status(201).sendFile(filteredpath);
   });
 
   // Root Endpoint
